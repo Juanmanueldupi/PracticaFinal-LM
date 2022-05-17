55b8109c840e82e948abbf06fdea32aed09dd761
juanmanueldupi <juanmadupi@gmail.com>
2022-05-17 02:03:57 +0200

    Programa valida numero

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 import requests
+import json
 import os
 from flask import Flask, render_template,abort
 app = Flask(__name__)	
 URL_BASE="https://phonevalidation.abstractapi.com/v1/"
 
-@app.route('/',methods=["GET","POST"])
-def inicio():
-    key=os.environ["key"]
-    payload = {'key':key}
-    r=requests.get(URL_BASE+'x',params=payload)
-    if r.status_code == 200:
-	    doc=r.json()
-        return render_template("inicio.html", lista=doc['x'])
-    else:
-        abort(404)
-port=os.environ["PORT"]
-app.run('0.0.0.0',  int(port), debug=False)
+#Antes de poder importar key tenemos que introducir en la terminal:
+#export api_key=<api_key>
+key=os.environ["api_key"]
+
+print ("Formato valido: pefijo+numero; Ej:34123456789")
+phone= input("Introduce tu numero de telefono:")
+payload = {'api_key':key , 'phone':phone}
+
+r=requests.get(URL_BASE,params=payload)
+
+if r.status_code == 200:
+    doc=r.json()
+    print (doc)
+else:
+    print ("Error, el telefono no es valido")
+
+
