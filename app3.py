884885d68c54b8ee96b9e39f0de4c3ee8c91ba39
juanmanueldupi <juanmadupi@gmail.com>
2022-05-22 03:40:24 +0200

    Finalizada 1ª parte

--- app3.py
+++ app3.py
@@ -10,6 +10,8 @@ r=requests.get(URL_BASE+pokemon)
 
 if r.status_code == 200:
     doc=r.json()
-    print (doc)
+    print("Id:",doc.get("id"))
+    print("Pokemon:",doc.get("name"))
+    print("Sprite:",doc.get("sprites").get("front_default"))
 else:
 	print("Error en la API")
